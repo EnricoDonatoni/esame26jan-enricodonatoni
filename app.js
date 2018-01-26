3630eb09e434e6bb35d5dcbbb2a96e21a483592d
Enrico Donatoni <enrico.donatoni@studenti.unitn.it>
2018-01-26 10:43:38 +0100

    modifica app.js per inviare parametri con richiesta POST alla funzione check, e ritorna un json

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 const express = require('express'),
     bodyParser = require('body-parser');
 
+var modulo = require("./checker.js");
+
+
 const app = express();
 app.use(bodyParser.json());
 
@@ -11,6 +14,17 @@ app.set('port', (process.env.PORT || 5000));
 // a useless function that returns a fixed object. you can use it, if you want, for testing purposes
 app.get('/count',function (req, res) {
     res.json({count: 5})
+})
+
+app.post('/check',function (req, res) {
+	
+	var url = req.body.url || req.query.url;
+	var invocationParameters = req.body.invocationParameters || req.query.invocationParameters;
+	var expectedResultData = req.body.expectedResultData || req.query.expectedResultData;
+	var expectedResultStatus = req.body.expectedResultStatus || req.query.expectedResultStatus;
+
+
+    res.json({res: modulo.check(url, invocationParameters, expectedResultData, expectedResultStatus)});
 })
 
 app.listen(app.get('port'), function() {
